7d411cb9586d50a0c2f9845493ac8819026a3d34
Nala Ginrut <mulei@gnu.org>
2021-02-21 12:47:09 +0800

    update

--- src/main.c
+++ src/main.c
@@ -1,4 +1,4 @@
-/*  Copyright (C) 2020-2021 LambdaChip
+/*  Copyright (C) 2020-2021
  *        "Mu Lei" known as "NalaGinrut" <NalaGinrut@gmail.com>
  *  LambdaChip is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License as
@@ -29,6 +29,68 @@ GLOBAL_DEF (size_t, VM_CODESEG_SIZE) = 8192;
 GLOBAL_DEF (size_t, VM_DATASEG_SIZE) = 0;
 GLOBAL_DEF (size_t, VM_STKSEG_SIZE) = 1024;
 
+#define DEFAULT_LEF "/SD:/program.lef"
+#define BUF_SIZE    512
+
+#if defined BOARD_LAMBDACHIP_ALONZO
+#  define LEF_LOADER load_lef_from_tf
+GLOBAL_DEF (const struct device *, dev_led0) = device_get_binding (LED0);
+GLOBAL_DEF (const struct device *, dev_led1) = device_get_binding (LED1);
+GLOBAL_DEF (const struct device *, dev_led2) = device_get_binding (LED2);
+GLOBAL_DEF (const struct device *, dev_led3) = device_get_binding (LED3);
+
+static void init_alonzo (void)
+{
+  int ret = 0;
+  /* Set LED pin as output */
+  ret
+    = gpio_pin_configure (dev_led0, LED0_PIN, GPIO_OUTPUT_ACTIVE | LED0_FLAGS);
+  if (ret < 0)
+    {
+      return;
+    }
+  ret
+    = gpio_pin_configure (dev_led1, LED1_PIN, GPIO_OUTPUT_ACTIVE | LED1_FLAGS);
+  if (ret < 0)
+    {
+      return;
+    }
+  ret
+    = gpio_pin_configure (dev_led2, LED2_PIN, GPIO_OUTPUT_ACTIVE | LED2_FLAGS);
+  if (ret < 0)
+    {
+      return;
+    }
+  ret
+    = gpio_pin_configure (dev_led3, LED3_PIN, GPIO_OUTPUT_ACTIVE | LED3_FLAGS);
+  if (ret < 0)
+    {
+      return;
+    }
+
+  gpio_pin_set (dev_led0, LED0_PIN, 1);
+  gpio_pin_set (dev_led1, LED1_PIN, 1);
+  gpio_pin_set (dev_led2, LED2_PIN, 1);
+  gpio_pin_set (dev_led3, LED3_PIN, 1);
+}
+#endif
+
+static lef_t load_lef_from_tf (void)
+{
+  lef_t lef = NULL;
+  u8_t buf[BUF_SIZE] = {0};
+
+  if (0 != mount_disk ())
+    {
+      printk ("Alonzo: TF card mount error!\n");
+      return NULL;
+    }
+
+  lef = load_lef_from_file (DEFAULT_LEF);
+
+  return lef;
+}
+
 void main (void)
 {
   // set BLE module to transparent send mode
@@ -41,5 +103,13 @@ void main (void)
    * 4. Add online DEBUG
    */
   printk ("Platform: zephyr on %s\n", CONFIG_BOARD);
-  lambdachip_start ();
+
+#if defined BOARD_LAMBDACHIP_ALONZO
+  init_alonzo ();
+  lambdachip_start (load_lef_from_file);
+#else
+  printk ("FATAL: LambdaChip was incorrectly configured, please check your "
+          "config!\n");
+  panic ("PANIC!");
+#endif
 }
